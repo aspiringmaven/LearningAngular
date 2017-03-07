4fb5c9789a5c5d5abf12dfc983bd49464c38193f
Sumit Kawatra <sumitkkawatra@gmail.com>
2017-03-07 12:02:37 +0530

    Angular JS | Practice Set 1 | Hello World

---
+++ App.js
@@ -0,0 +1,14 @@
+var myApp = angular.module("AppModule",[]);
+
+myApp.controller('AppController',['$scope',function($scope){
+
+$scope.product = {
+
+id : "12",
+name : "Sumit Kawatra",
+detail : "Ye hai Details.."
+
+}
+
+
+}]);
